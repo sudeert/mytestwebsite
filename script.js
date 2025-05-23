a02cd75b538449a921ffd88e2b0784f69eb873a5
sudeert <154585344+sudeert@users.noreply.github.com>
2025-05-23 17:32:28 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,3 @@
+document.getElementById('helloBtn').addEventListener('click', function() {
+    document.getElementById('message').textContent = 'Thanks for clicking! Welcome to Azure Static Web Apps!';
+});
